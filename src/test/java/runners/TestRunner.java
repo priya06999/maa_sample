8a139ebd438716d9531cb26455bf37052bb12a79
Kaki, Gnaneswari <Gnaneswari.Kaki@citizensbank.com>
2022-09-07 22:57:27 -0500

    checking changes

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -7,8 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(plugin = {
         "pretty",
         "html:target/cucumber-report/cucumber.html",
-        "json:target/cucumber-report/cucumber.json",
-        "html:io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
+        "json:target/cucumber-report/cucumber.json"
 
 },
         features = "src/test/resources/features",
